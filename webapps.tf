a115650e292deb4634e4d48effb27773d87f2284
Richard Cheney <richard.cheney@microsoft.com>
2018-06-11 18:39:47 +0100

    Added outputs

--- webapps.tf
+++ webapps.tf
@@ -35,3 +35,7 @@ resource "azurerm_app_service" "citadel" {
 
     app_service_plan_id = "${element(azurerm_app_service_plan.free.*.id, count.index)}"
 }
+
+output "webappids" {
+  value = "${azurerm_app_service.citadel.*.id}"
+}
