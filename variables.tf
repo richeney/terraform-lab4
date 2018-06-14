d7472ed5f623a6c5d9b9b28aa6a12acdd2402cbe
Richard Cheney <richard.cheney@microsoft.com>
2018-06-14 15:49:16 +0100

    Added tfvars

--- variables.tf
+++ variables.tf
@@ -12,22 +12,14 @@ variable "tags" {
 
 variable "tenant_id" {
     description = "GUID for the Azure AD tenancy or directory"
-    default     = "72f988bf-86f1-41af-91ab-2d7cd011db47"
 }
 
 variable "object_id" {
     description = "Object ID for the terraformKeyVaultReader service principal"
-    default     = "4e12ff32-c439-49aa-adaf-026ff6366576"
 }
 
 variable "webapplocs" {
     description = "List of locations for web apps"
     type        = "list"
-    default     = [
-        "westeurope",
-        "centralindia",
-        "westus2",
-        "australiaeast",
-        "brazilsouth"
-    ]
+    default     = []
 }
